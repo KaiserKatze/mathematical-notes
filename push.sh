e6f87c0b23e6d3434ce543f346f9924f05dfa17a
KaiserKatze <donizyo@gmail.com>
2023-07-06 15:23:49 +0800

    Update

--- push.sh
+++ push.sh
@@ -10,7 +10,8 @@ eval $(ssh-agent) && ssh-add ||\
 	(echo "[ERROR] Fail to start ssh agent deamon or add ssh key!" 1>&2 && exit 1)
 
 OUTPUT_FILE="数学笔记.pdf"
-cp math.pdf "$OUTPUT_FILE"  # 在本地重命名 texlive 输出的 PDF 文件
+cp math.pdf "$OUTPUT_FILE" &&\  # 在本地重命名 texlive 输出的 PDF 文件
+	echo "[INFO] 已生成最新版'数学笔记.pdf'文件."
 
 read -p "是否向 Github 上传 PDF 文件？(Y/n) " do_upload_pdf_file
 
@@ -26,7 +27,7 @@ esac
 # 开始推送
 for remote in $(git remote); do
 	echo "[INFO] Start pushing to '$(git remote get-url $remote)' ..."
-	git push $remote && git push --tags $remote ||\
+	git push -f $remote && git push -f --tags $remote ||\
 		echo "[ERROR] Fail git push $remote!" 1>&2
 done
 
