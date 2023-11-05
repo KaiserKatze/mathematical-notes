2521c6d318b19e772e8984745badebc846fe2965
KaiserKatze <donizyo@gmail.com>
2023-11-05 14:18:03 +0800

    Update

--- tex-linter.py
+++ tex-linter.py
@@ -10,16 +10,26 @@ logger.setLevel(logging.INFO)
 
 info_log_handler = logging.StreamHandler()
 info_log_handler.setLevel(logging.INFO)
-info_log_handler.setFormatter(logging.Formatter("\033[32m%(levelname)s %(message)s\033[0m"))
+info_log_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
 info_log_handler.addFilter(lambda record: record.levelno == logging.INFO)
 logger.addHandler(info_log_handler)
 
 error_log_handler = logging.StreamHandler()
 error_log_handler.setLevel(logging.ERROR)
-error_log_handler.setFormatter(logging.Formatter("\033[31m%(levelname)s %(message)s\033[0m"))
+error_log_handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
 error_log_handler.addFilter(lambda record: record.levelno == logging.ERROR)
 logger.addHandler(error_log_handler)
 
+is_tex_project_problematic = False
+error_counter = 0
+total_line_counter = 0
+
+def error(msg, _file, _line_num, _prev_line, _line):
+    logger.error("{}\n{} 第{}行：\n\t{}>>>\t{}".format(msg, _file, _line_num, _prev_line, _line))
+    global is_tex_project_problematic
+    global error_counter
+    is_tex_project_problematic = True
+    error_counter += 1
 
 def find_all_tex_files():
     for dirpath, dirnames, filenames in os.walk('.'):
@@ -29,24 +39,82 @@ def find_all_tex_files():
             if filename.endswith(".tex"):
                 yield os.path.join(dirpath, filename)
 
+def scan_math_environments(_file):
+    with open(_file, mode='r', encoding="utf-8") as fd:
+        prev_line = ""
+        lines = iter(fd.readlines())
+        line_num = 0
+
+        line_num_begin = 0
+        line_begin = None
+        line_num_end = 0
+        line_end = None
+
+        def report():
+            if not (line_num_begin < line_num_end):
+                return
+            error("【0008】在数学公式环境中存在中文全角标点符号！\n"
+                    "环境参数【起：{}，止：{}】\n"
+                    "起 >>> {}\n"
+                    "止 >>> {}".format(line_num_begin, line_num_end, line_begin, line_end),
+                    file, line_num, prev_line, line)
+
+        def check(fn_begin, fn_end, _line_num, _line):
+            nonlocal line_num_begin
+            nonlocal line_num_end
+            nonlocal line_begin
+            nonlocal line_end
+            line_num_begin = 0
+            line_begin = None
+            line_num_end = 0
+            line_end = None
+            if fn_begin(_line):
+                logger.debug("{} 第{}行：数学公式环境 开始 >>> {}".format(file, _line_num, line))
+                line_num_begin = _line_num
+                line_begin = _line
+                for _line_ in lines:
+                    _line_num += 1
+                    logger.debug("正在扫描 {} 第{}行 >>> {}".format(file, _line_num, _line_))
+                    if fn_end(_line_):
+                        logger.debug("{} 第{}行：数学公式环境 关闭 >>> {}".format(file, _line_num, line))
+                        line_num_end = _line_num
+                        line_end = _line_
+                        break
+                    if re.search(r"[\uff09\uff0c\uff1b]", _line_):
+                        report()
+                return True
+
+        for line in lines:
+            line_num += 1
+            logger.debug("正在扫描 {} 第{}行 >>> {}".format(file, line_num, line))
+            check(lambda l: re.search(r"\\\[", l),
+                    lambda l: re.search(r"\\\]", l),
+                    line_num, line)
+            for env_name in [
+                "align",
+                "align*",
+                "gather",
+                "gather*",
+            ]:
+                if check(lambda l: re.search(r"\\begin\{"+env_name+r"\}", l),
+                            lambda l: re.search(r"\\end\{"+env_name+r"\}", l),
+                            line_num, line):
+                    break  # 退出循环
+            prev_line = line
+
+
 # `\uff09` 是全角右圆括号 ）
 # `\uff0c` 是全角逗号 ，
 # `\uff1b` 是全角分号 ；
 if __name__ == "__main__":
-    is_tex_project_problematic = False
-    error_counter = 0
-
-    def error(msg, _file, _line_num, _prev_line, _line):
-        logger.error("{}\n{} 第{}行：\n\t{}>>>\t{}".format(msg, _file, _line_num, _prev_line, _line))
-        global is_tex_project_problematic, error_counter
-        is_tex_project_problematic = True
-        error_counter += 1
-
     for file in find_all_tex_files():
+        scan_math_environments(file)
+
         with open(file, mode='r', encoding="utf-8") as fd:
             prev_line = ""
             for line_num, line in enumerate(fd.readlines()):
                 line_num += 1  # enumerate 函数给出的序号是从0开始的，行号是从1开始的，进行修正
+                total_line_counter += 1
 
                 if re.search(r"\s+\\cref\b", line):
                     error("【0001】\cref 命令前不应有空格！", file, line_num, prev_line, line)
@@ -97,6 +165,7 @@ if __name__ == "__main__":
                 prev_line = line
     if not is_tex_project_problematic:
         logger.info("检查完毕，没有任何错误！")
+        logger.info("本次扫描总计{}行.".format(total_line_counter))
     else:
         logger.error("共计发现{}条错误！请注意检查你的文件！".format(error_counter))
         exit(1)
